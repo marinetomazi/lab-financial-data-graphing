7b2f219dcbbe3eb21740e7a13000e5e2dcfd48c7
marwa.brj@gmail.com <marwa.brj@gmail.com>
2020-09-15 23:15:11 +0200

    iteration 2 done

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,6 +1,29 @@
 axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
 .then(response =>{
-    console.log(response.data.bpi, 'BPI OK')
+    console.log(response.data, 'BPI OK');
+    printTheChart( response.data.bpi)
 })
-.catch(err => console.log(err)
-)
+.catch(err => console.log(err))
+
+
+function printTheChart(stockData) {
+  const dates = Object.keys(stockData);
+  const bitCoins = dates.map(date => stockData[date]);
+  console.log('bitcoins:',bitCoins);
+  const ctx = document.getElementById('my-chart').getContext('2d');
+  const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: dates,
+      datasets: [
+        {
+          label: 'BitCoin Value',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: bitCoins
+        }
+      ]
+    }
+  }); // closes chart = new Chart()
+} // closes printTheChart()
+const stockPrices = stockDates.map(date => dailyData[date]['4. close']);
